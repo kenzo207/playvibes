2e9462bc9402bd0ee583e82ad161ae5bdd053e4f
kenzo207 <kenzo.hounkpe@epitech.eu>
2025-12-15 12:16:59 +0100

    fix: add missing spotify image domains to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,26 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
+        hostname: "mosaic.scdn.co",
+      },
+      {
+        protocol: "https",
+        hostname: "wrapped-images.spotifycdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "seed-mix-image.spotifycdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "image-cdn-ak.spotifycdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "image-cdn-fa.spotifycdn.com",
+      },
+      {
+        protocol: "https",
         hostname: "images.unsplash.com",
       },
     ],
